a922c641e77724e1e31a7397147b5599b8584635
AMIRJOON777 <167329397+AMIRJOON777@users.noreply.github.com>
2024-04-17 18:01:49 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
+let userID = 'de8c28f4-f968-4cf1-8b64-32d7e3fd29ae';
 
 const proxyIPs = ["workers.cloudflare.cyou"];
 
